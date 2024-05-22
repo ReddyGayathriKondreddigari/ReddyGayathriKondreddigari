baca46f37969bb338f0b8466d992cbbb7088ebdd
Reddy Gayathri <reddygayathri552@gmail.com>
2024-05-22 21:14:19 +0530

    Commit Message

---
+++ eclipse-workspace/PizzaBillGenerator/src/com/javaprojects/DeluxPizza.java
@@ -0,0 +1,24 @@
+package com.javaprojects;
+
+public class DeluxPizza  extends pizza{
+
+	public DeluxPizza(boolean veg) {
+		super(veg);
+		super.addExtraCheese();
+		super.addExtraToppings();
+	}
+
+	@Override
+	public void addExtraCheese() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addExtraToppings() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
---
+++ eclipse-workspace/PizzaBillGenerator/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author REDDY GAYATHRI
+ *
+ */
+module PizzaBillGenerator {
+}
